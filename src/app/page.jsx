ae4090302138c267997d1a0dc4fe5ff104c1a653
Mohd Rehan Raza <97344473+rehanraza788@users.noreply.github.com>
2026-01-29 11:52:08 +0530

    change button name

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -106,7 +106,7 @@ const Page = () => {
         onClick={() => setShowForm(true)}
         className="bg-green-600 text-white px-4 py-2 rounded"
       >
-        Add User
+        Add Person
       </button>
 
       {showForm && (
